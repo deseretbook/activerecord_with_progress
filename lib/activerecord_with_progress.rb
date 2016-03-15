5ca53ab7b2fd0cdda5a919fc28417dd2c1b531b8
Mike Bourgeous <mike@mikebourgeous.com>
2016-03-15 17:10:13 -0600

    Added ProgressBar gem setup.

--- lib/activerecord_with_progress.rb
+++ lib/activerecord_with_progress.rb
@@ -1,5 +1,22 @@
 require "activerecord_with_progress/version"
 
+require 'ruby-progressbar'
+
 module ActiverecordWithProgress
-  # Your code goes here...
+  # Default progress bar format (see ruby-progressbar documentation).
+  DEFAULT_FORMAT = "\e[36m%a \e[35m%e\e[0m \e[34m[\e[1m%B\e[0;34m] %c/%C %p%%\e[0m"
+  @progress_format = DEFAULT_FORMAT
+
+  class << self
+    # Set or retrieve the format string used for progress bars.
+    attr_accessor :progress_format
+
+    # Creates a progress bar with our own default options and format, unless
+    # overrided by the options hash.
+    def create(options=nil)
+      opts = { format: progress_format }
+      opts.merge!(options) if options.is_a?(Hash)
+      ProgressBar.create(opts)
+    end
+  end
 end
